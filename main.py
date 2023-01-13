a3c705aa12bca4a4b8a4264564ae350c07de4a67
dancingalfred <robban021@hotmail.com>
2023-01-13 14:29:44 +0100

    api-test

---
+++ main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+app = FastAPI()
+
+def test_function(text:str):
+    response = f"Your text is: {text}"
+    return response
+
+
+
+
+@app.get("/jobapi")
+def api(text:str):
+    response = test_function(text)
+    return {"Data": "Test", "Text response": response}
+
