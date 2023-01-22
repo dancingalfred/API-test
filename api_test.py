0605377359a0b029623c8025cfa6182611cdd052
dancingalfred <robban021@hotmail.com>
2023-01-22 18:30:18 +0100

    got secondt test to work, but not really

--- api_test.py
+++ api_test.py
@@ -1,15 +1,11 @@
 from fastapi import FastAPI
 import uvicorn
 
-
 app = FastAPI()
 
 def test_function(text:str):
     response = f"Your text is: {text}"
     return response
-
-
-
 
 @app.get("/jobapi")
 def api(text:str):
--- test_the_api.py
+++ test_the_api.py
@@ -1,20 +1,52 @@
-import requests
+# import requests
 
-apiLink = "https://jobdescrimination.azurewebsites.net/jobapi"
-myText = "Hej på dig"
+# apiLink = "https://jobdescrimination.azurewebsites.net/jobapi"
+# myText = "Hej på dig"
+
+# def get_data(api,text):
+#     response = requests.get(api,params={"text":text})
+
+
+#     if response.status_code == 200:
+#         print("sucessfully fetched the data")
+#         response = response.json()
+#         print(response)
+#     else:
+#         print(f"Hello person, there's a {response.status_code} error with your request")
+
+# get_data(apiLink,myText)
+
+
+import requests
+import os
+import glob
+import json
+
+apiLink = "http://127.0.0.1:8000/jobapi/{text}"
+
+
 
 def get_data(api,text):
-    response = requests.get(api,params={"text":text})
-
+    #response = requests.get(api,params={"text":text})
+    # response = requests.get(api.format(text=text))
+    response = requests.get(api, params={'text':text})
 
     if response.status_code == 200:
         print("sucessfully fetched the data")
-        response = response.json()
+        response = json.loads(response.text)
+
         print(response)
     else:
         print(f"Hello person, there's a {response.status_code} error with your request")
 
-get_data(apiLink,myText)
+#get_data(apiLink,myText)
 
 
 
+folder_path = './Job_Bulletins/'
+
+
+for file_path in glob.glob(os.path.join(folder_path, '*.txt')):
+    with open(file_path, 'r') as file:
+        myText = file.read()
+        get_data(apiLink, myText)
