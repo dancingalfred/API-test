c99577413cbecf5b8656d70074b670ab2589e917
dancingalfred <robban021@hotmail.com>
2023-01-23 11:30:07 +0100

    tested if male and female exist

--- second_test.py
+++ second_test.py
@@ -6,15 +6,19 @@ import json
 
 folder_path = './Job_Bulletins/'
 
-
+results = []
 for file_path in glob.glob(os.path.join(folder_path, '*.txt')):
     with open(file_path, 'r') as file:
         text = file.read()
-        response = requests.get(f"http://localhost:8000/jobapi",params = {"text":text})
+        response = requests.get(f"http://localhost:8000/jobapi/",params = {"text":text})
         if response.status_code == 200:
             result = response.json()["Result"]
             print(result)
+            if result not in results:
+                results.append(result)
+            else:
+                continue
         else:
             print(f"Request failed with status code {response.status_code}")
 
-
+print(results)
